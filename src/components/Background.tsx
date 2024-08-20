3d71be4d078375eaef35476ef6ac6819bfdddf97
Marcos Bérgamo <marcos@thedon.com.br>
2024-08-20 11:20:03 -0300

    fix: change background component bg color

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,10 +1,11 @@
 import { cn } from '@/utils/Helpers';
 
+/* FIXME: update with the design system later */
 const Background = (props: {
   children: React.ReactNode;
   className?: string;
 }) => (
-  <div className={cn('w-full bg-secondary', props.className)}>
+  <div className={cn('w-full bg-[#F9B8AB]', props.className)}>
     {props.children}
   </div>
 );
